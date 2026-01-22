e3581886c8ae58b8773dfff4df55844946c86d64
Santigo Viñan <santigovinan@Santigos-MacBook-Pro.local>
2026-01-22 12:09:55 -0500

    cambios

--- surveys/urls.py
+++ surveys/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from adiccionestic.views import ExportInterfaceView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
